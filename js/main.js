87cf59fda93457bf5d9129aade9bcdc656da674e
junior783 <andrejr.83@gmail.com>
2018-02-17 21:50:57 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// init Isotope
+var $container = $('.portfolio-items').isotope({});
+
+// filter items on button click
+$('.portfolio-filter').on( 'click', 'a', function(e) {
+	e.preventDefault();
+  	var filterValue = $(this).attr('data-filter');
+  	$container.isotope({ filter: filterValue });
+
+  	$('.portfolio-filter li').removeClass('active');
+  	$(this).closest('li').addClass('active')
+});
+
+
